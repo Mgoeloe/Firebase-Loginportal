cd819d0f6378f150fcf7566a1e5e0e80c442ecfb
Marlon Goeloe <74913825+Mgoeloe@users.noreply.github.com>
2021-03-24 17:09:00 +0100

    Inplemented SignUp

--- Backup/SignUp/index.js
+++ Backup/SignUp/index.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
-import { Link, withRouter } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
-import {withFirebase} from '../Firebase';
+import {FirebaseContext} from '../Firebase';
 import * as ROUTES from '../../constants/routes';
 
 // Hook van maken??
@@ -9,7 +9,10 @@ const SignUpPage = () => {
     return (
         <div>
             <h1>SignUp</h1>
-            <SignUpForm />}
+            <FirebaseContext.Consumer>
+                {firebase => <SignUpForm firebase={firebase} />}
+            </FirebaseContext.Consumer>
+            
         </div>
     )
 };
@@ -22,21 +25,20 @@ const INITIAL_STATE = {
     error: null,
 };
 
-class SignUpFormBase extends Component {
+class SignUpForm extends Component {
     constructor(props) {
         super(props);
 
         this.state = { ...INITIAL_STATE };
     }
 
-    onSubmit = (event) => {
+    onSubmit = event => {
         const { username, email, passwordOne } = this.state;
 
         this.props.firebase
             .doCreateUserWithEmailAndPassword(email, passwordOne)
             .then(authUser => {
                 this.setState({ ...INITIAL_STATE });
-                this.props.history.push(ROUTES.HOME);
             })
             .catch(error => {
                 this.setState({ error });
@@ -103,15 +105,6 @@ class SignUpFormBase extends Component {
         );
     }
 }
-
-const SignUpLink = () => (
-    <p>
-      Don't have an account? <Link to={ROUTES.SIGN_UP}>Sign Up</Link>
-    </p>
-);
-
-const SignUpForm = withRouter(withFirebase(SignUpFormBase));
-
 
 export default SignUpPage;
 
